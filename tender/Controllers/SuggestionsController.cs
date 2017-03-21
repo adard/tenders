b49400b2e68e1d6dec78db30503c71bd55295f9a
hannahbenhamou94 <adardayan@gmail.com>
2017-03-21 20:46:56 +0200

    first cimmit

---
+++ tender/Controllers/SuggestionsController.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using tender.Models;
+
+namespace tender.Controllers
+{
+    public class SuggestionsController : Controller
+    {
+        // GET: Suggestions
+        public ActionResult Suggestions()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Suggestions(FormCollection ff)
+        {
+            return View();
+        }
+
+
+        public ActionResult HollandSuggest()
+        {
+            return View();
+        }
+
+        public ActionResult stopTender(string numTender)
+        {
+
+            try
+            {
+                using (DbtenderEntities1 DB = new DbtenderEntities1())
+                {
+                    var tender = DB.Tenders.Find(Convert.ToInt32(numTender));
+                    tender.status = "סגור";
+                    DB.SaveChanges();
+                }
+            }
+            catch (Exception) { }
+            return View();
+        }
+
+
+        public ActionResult ChangePrice(int numtender)
+        {
+            DateTime dt = DateTime.Now;
+            List<ProducToTender> products = new List<ProducToTender>();
+            using (DbtenderEntities1 DB = new DbtenderEntities1())
+            {
+                products = DB.ProducToTender.Where(a => a.numTender.Equals(numtender)).OrderBy(a => a.numTender).ToList();
+            }
+            //  return Json(ProducToTender, JsonRequestBehavior.AllowGet);
+            if (products != null)
+            {
+                try
+
+
+                {
+                    foreach (var item in products)
+                    {
+
+                        using (DbtenderEntities1 DB = new DbtenderEntities1())
+                        {
+                            var tender = DB.ProducToTender.Find((item.TenderDetailsID));
+
+                            tender.PriceUpdate = item.PriceUpdate - (float)item.sizeRoomy;
+                            tender.DateUpdate = dt;
+                            DB.SaveChanges();
+                        }
+                    }
+                }
+
+          
+                catch (Exception) { }
+            }
+            return View();
+        }
+
+
+        public ActionResult getSuggestionDetail(int numTender)
+        {
+
+            DbtenderEntities1 DB = new DbtenderEntities1();
+
+
+            var status = from t in DB.Tenders
+                         where t.numTender == numTender
+                         select t.status;
+            if (status.ToList()[0].ToString().Contains("סגור"))
+            {
+                return Json("close", JsonRequestBehavior.AllowGet);
+
+            }
+
+            //return View();
+            var result = from p in DB.ProducToTender
+                         join t in DB.Tenders on p.numTender equals t.numTender
+                         where p.numTender == numTender
+                         orderby p.NameProduct
+                         select new { p.numProduct, p.NameProduct, p.Amount, p.PriceUpdate, p.sizeRoomy };
+
+
+            //return View();
+
+            return Json(result.Distinct().ToList(), JsonRequestBehavior.AllowGet);
+
+        }
+        public JsonResult save(string str)
+        {
+            return Json("success:saved", JsonRequestBehavior.AllowGet);
+        }
+
+
+
+        //[HttpPost]
+        //public JsonResult save(Suggestions s1)
+        //{
+        //    bool status = false;
+        //    TimeSpan dateOrgF;
+        //    var isValidDateFrom = DateTime.TryParseExact(s1.timeSuggestion.ToString(), "mm-dd-yyyy", null, System.Globalization.DateTimeStyles.None, out dateOrgF);
+        //    if (isValidDateFrom)
+        //    {
+        //        s1.timeSuggestion = dateOrgF;
+        //    }
+
+
+        //    var isValidModel = TryUpdateModel(s1);
+        //    if (isValidModel)
+        //    {
+        //        using (DbtenderEntities DB = new DbtenderEntities())
+        //        {
+        //            //foreach (var item in Tenders.ProducToTender)
+        //            //{
+        //            //    DB.ProducToTender.Add(item) ;
+        //            //}
+        //            //DB.SaveChanges();
+
+        //            DB.Suggestions .Add(s1);
+        //            DB.SaveChanges();
+        //            status = true;
+
+        //        }
+        //    }
+        //    return new JsonResult { Data = new { status = status } };
+        //}
+
+        public ActionResult getTender(int numTender)
+        {
+
+            List<Tenders> Tender = new List<Tenders>();
+            using (DbtenderEntities1 DB = new DbtenderEntities1())
+            {
+                Tender = DB.Tenders.Where(a => a.numTender.Equals(numTender)).OrderBy(a => a.numTender).ToList();
+            }
+            return Json(Tender, JsonRequestBehavior.AllowGet);
+
+        }
+        public ActionResult getProduct(int numTender)
+        {
+
+            List<ProducToTender> products = new List<ProducToTender>();
+            using (DbtenderEntities1 DB = new DbtenderEntities1())
+            {
+                products = DB.ProducToTender.Where(a => a.numTender.Equals(numTender)).OrderBy(a => a.numTender).ToList();
+            }
+            return Json(products, JsonRequestBehavior.AllowGet);
+
+        }
+        public ActionResult getType(int numType)
+        {
+            List<TypeTender> type = new List<TypeTender>();
+            using (DbtenderEntities1 DB = new DbtenderEntities1())
+            {
+                type = DB.TypeTender.Where(a => a.numType.Equals(numType)).OrderBy(a => a.nameType).ToList();
+            }
+            return Json(type, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult getSuggestion(int numTender)
+        {
+
+            List<Suggestions> products = new List<Suggestions>();
+            using (DbtenderEntities1 DB = new DbtenderEntities1())
+            {
+                products = DB.Suggestions.Where(a => a.numTender.Equals(numTender)).OrderBy(a => a.numSuggestion).ToList();
+            }
+            return Json(products, JsonRequestBehavior.AllowGet);
+
+        }
+
+        public ActionResult allSuggestion(int numTender)
+        {
+
+            //if (Request.QueryString["id"] == null)
+            //    return View();
+            //int id = Convert.ToInt32(Request.QueryString["id"]);
+            DbtenderEntities1 DB = new DbtenderEntities1();
+
+            var result = from t in DB.Tenders
+                         join s in DB.Suggestions on t.numTender equals s.numTender
+                         join s1 in DB.SuggestionDetail on s.numSuggestion equals s1.numsuggest
+                         join p in DB.ProducToTender on t.numTender equals p.numTender
+
+                         where t.numTender == numTender
+                         orderby s1.numproduct
+                         select new { t.numTender, s1.numproduct, p.PriceUpdate, p.sizeRoomy };
+
+
+
+
+
+            //return View();
+
+            return Json(result.ToList(), JsonRequestBehavior.AllowGet);
+
+        }
+
+        public ActionResult getPrice(int numSuggestion)
+        {
+
+            List<SuggestionDetail> products = new List<SuggestionDetail>();
+            using (DbtenderEntities1 DB = new DbtenderEntities1())
+            {
+                products = DB.SuggestionDetail.Where(a => a.numsuggest.Equals(numSuggestion)).OrderBy(a => a.priceToProduct).ToList();
+            }
+            return Json(products, JsonRequestBehavior.AllowGet);
+
+        }
+
+        public JsonResult getAllSuggestions(int numtender)
+        {
+            List<Suggestions> suggestion = new List<Suggestions>();
+            using (DbtenderEntities1 DB = new DbtenderEntities1())
+            {
+                suggestion = DB.Suggestions.Where(a => a.numTender.Equals(numtender)).OrderBy(a => a.numSuggestion).ToList();
+            }
+            return new JsonResult { Data = suggestion, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+
+        public JsonResult getSuggestionDetails(int numsuggestion)
+        {
+            List<SuggestionDetail> suggestionDetail = new List<SuggestionDetail>();
+            using (DbtenderEntities1 DB = new DbtenderEntities1())
+            {
+                suggestionDetail = DB.SuggestionDetail.Where(a => a.numDetailSuggestion.Equals(numsuggestion)).OrderBy(a => a.numDetailSuggestion).ToList();
+            }
+            return new JsonResult { Data = suggestionDetail, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+
+        public ActionResult Loadtime(int numtender)
+        {
+            DbtenderEntities1 DB = new DbtenderEntities1();
+
+            var result = from p in DB.ProducToTender
+                         join t in DB.Tenders on p.numTender equals t.numTender
+                         where p.numTender == numtender
+                         select new { p.DateUpdate,t.time_update};
+            
+            //   var t=(Convert.ToDateTime(result));
+            //  var tender = DB.Tenders.Find(Convert.ToInt32(numTender));
+
+            // var s =t.ToString.conver
+            //return View();
+         //   DateTime dt = result.ToList.;
+             return Json(result.Distinct().ToList(), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult getSuggestionMax(int numTender)
+        {
+
+            DbtenderEntities1 DB = new DbtenderEntities1();
+
+
+            var status = from t in DB.Tenders
+                         where t.numTender == numTender
+                         select t.status;
+            if (status.ToList()[0].ToString().Contains("סגור"))
+            {
+                return Json("close", JsonRequestBehavior.AllowGet);
+
+            }
+
+            //return View();
+            var result = from s in DB.Suggestions
+                         join p in DB.ProducToTender on s.numTender equals p.numTender
+                         join s1 in DB.SuggestionDetail on s.numSuggestion equals s1.numsuggest
+                         where s.numTender == numTender
+                         orderby s1.numproduct
+                         select new {p.sizeRoomy,p.numTender,p.numProduct };
+
+
+            //return View();
+
+            return Json(result.Distinct().ToList(), JsonRequestBehavior.AllowGet);
+
+        }
+
+
+    }
+}
---
+++ tender/Scripts/HollandSuggestions.js
@@ -0,0 +1,270 @@
+﻿var url = null;
+var sRommy = [];
+var numproduct = [];
+var myvar;
+var prices = [];
+var flag = 0;
+
+function time(x,time) {
+    var diff = (time - x) * 1000;
+ //   alert(diff+" diff");
+  setTimeout(function () {
+   //   alert("this the message for fun set time out");
+  },diff);
+  timeInterval(time);
+}
+
+function timeInterval(time)
+{
+    var setTime = time * 1000;
+  //  alert(time + " time timer");
+    setInterval(function () {
+        $.ajax({
+            type: "POST",
+            url: "/Suggestions/ChangePrice",
+            data: { 'numTender': url },
+            success: function (data) {
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        })
+    LoadAgain();
+    }, (120000));
+}
+
+
+
+function changePrice(x) {
+   
+    for (var i = 0; i < x; i++){
+        if (!url)
+        {
+            url = window.location.href;
+            url = url.substr(url.indexOf('=') + 1);
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "/Suggestions/ChangePrice",
+            data: { 'numTender': url },
+            success: function (data) {
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        })
+    }//for
+
+    LoadAgain();
+
+    }
+
+
+function LoadTime()
+    {
+    if (!url) {
+        url = window.location.href;
+        url = url.substr(url.indexOf('=') + 1);
+
+    }
+    //alert(url);
+    $.ajax({
+        type: "POST",
+        url: "/Suggestions/LoadTime",
+        dataType: 'json',
+        data: { 'numTender': url },
+        success: function (data) {
+            updateTime(data);
+
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    })
+}
+
+function LoadAgain() {
+    if (!url) {
+        url = window.location.href;
+        url = url.substr(url.indexOf('=') + 1);
+
+    }
+    //alert(url);
+    $.ajax({
+        type: "POST",
+        url: "/Suggestions/getSuggestionDetail",
+        dataType: 'json',
+        data: { 'numTender': url },
+        success: function (data) {
+            //render products to appropriate dropdown
+            var x = data.includes("close");
+            if (!x) {
+                renderAgain(data);
+            }
+            else {
+                document.getElementById("dd").innerHTML = "המכרז סגור";
+            }
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    })
+}
+
+function renderAgain(data) {
+  //  alert("again");
+  
+      $.each(data, function (i, v) {
+          
+
+          var table = document.getElementById("t01");
+          //table.rows[i].cells[0].innerHTML = v.NameProduct;
+          //table.rows[i].cells[1].innerHTML = v.PriceUpdate;
+          //table.rows[i].cells[2].innerHTML = v.Amount;
+           row = table.insertRow(i + 1);
+           cell1 = row.insertCell(0);
+           cell2 = row.insertCell(1);
+           cell3 = row.insertCell(2);
+
+          cell1.innerHTML = v.NameProduct;
+          cell2.innerHTML = v.PriceUpdate;
+          cell3.innerHTML = v.Amount;
+
+     
+
+      })
+
+}
+
+
+
+function LoadDTender() {
+    if (!url) {
+        url = window.location.href;
+        url = url.substr(url.indexOf('=') + 1);
+
+    }
+    //alert(url);
+    $.ajax({
+        type: "POST",
+        url: "/Suggestions/getSuggestionDetail",    
+        dataType:   'json',
+        data: { 'numTender': url },
+        success: function (data) {
+            //render products to appropriate dropdown
+            var x = data.includes("close");
+            if (!x) {
+                renderDTender(data);
+            }
+            else {
+                document.getElementById("dd").innerHTML = "המכרז סגור";
+            }
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    })
+}
+
+function renderDTender(data) {
+    LoadTime();
+
+        //render product
+        $.each(data, function (i, v) {
+            var table = document.getElementById("t01");
+            var row = table.insertRow(i + 1);
+            var cell1 = row.insertCell(0);
+            var cell2 = row.insertCell(1);
+            var cell3 = row.insertCell(2);
+
+            cell1.innerHTML = v.NameProduct;
+            cell2.innerHTML = v.PriceUpdate;
+            cell3.innerHTML = v.Amount;
+        })
+}
+
+
+function updateTime(data) {
+    var time_update;
+    $.each(data, function (i, v) {
+        //
+        if (flag == 0) {
+            var my_date = ConvertJSONDateToDate(v.DateUpdate);
+            //alert(JSON.stringify(v.time_update));
+            var j= JSON.stringify(v.time_update);
+            var json = $.parseJSON(j);
+            $(json).each(function (i, val) {
+                $.each(val, function (k, v) {
+                    if (k == "TotalMilliseconds")
+                        time_update = v;
+                });
+            });
+            var diff = checkTime(my_date);
+            //var count = diff + 1;
+            var count =Math.floor( diff / time_update);
+         //   alert(count + " count");
+            if (count > 0)
+            {
+            //    alert("update time and the count  " + count);
+                changePrice(count);
+            }
+            var module = diff % time_update;
+         //   alert(module + "  module");
+
+            time(module,time_update);
+         //   alert(time_update + " time ");
+            flag = 1;
+
+
+        }
+    })
+
+ 
+}
+
+
+function ConvertJSONDateToDate(jsonDate) {
+    var dateSlice = jsonDate.slice(6, 24);
+    var milliseconds = parseInt(dateSlice);
+    var date = new Date(milliseconds);
+    return date;
+}
+
+function checkTime(s)
+{
+    var date = new Date();
+    var diff = Math.abs(date - s);
+    return diff;
+
+}
+function ToJavaScriptDate(value) { //To Parse Date from the Returned Parsed Date
+    var pattern = /Date\(([^)]+)\)/;
+    var results = pattern.exec(value);
+    var dt = new Date(parseFloat(results[1]));
+    return (dt.getMonth() + 1) + "/" + dt.getDate() + "/" + dt.getFullYear();
+}
+function ToJavaScriptTime(value) { //To Parse Date from the Returned Parsed Date
+    var pattern = /Date\(([^)]+)\)/;
+    var results = pattern.exec(value);
+    var dt = new Date(parseFloat(results[1]));
+    return (dt.getHours()) + ":" + dt.getMinutes() + ":" + dt.getSeconds() + ":" + dt.getMilliseconds();
+}
+    function stopTender() {
+
+        $.ajax({
+            type: "POST",
+            url: "/Suggestions/stopTender",
+            data: { 'numTender': url },
+            success: function () {
+               // alert("Tender stoped!");
+                $("#stop").Attr('disabled');
+                //render products to appropriate dropdown
+                // renderDTender(data);
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        })
+    }
+    LoadDTender();
